9450c6f40bbf1f4d5f9a323a44b1c78929a0a7aa
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-03-08 16:30:14 +0900

    :+1: Edit .gitignore 6

--- diary-app/GoalViewController.swift
+++ diary-app/GoalViewController.swift
@@ -13,10 +13,7 @@ class GoalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
 
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
