afd56f90d39f0a45cffe27b869d65a2c29ec5181
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-01 23:51:12 +0900

    :sparkles: Add login.xcodeproj (sample)

---
+++ sampleapps/login/login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Yuka Okada on 2021/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
