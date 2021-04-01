4c0a72c8228d2d5646bef5556fe59f67307e1be6
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-02 01:16:47 +0900

    :sparkles: Add support for signin (login-sample)

--- sampleapps/login/login/ViewController.swift
+++ sampleapps/login/login/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import AWSMobileClient
+import AWSAppSync
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        // Do any additional setup after loading the view.
+        AWSMobileClient.sharedInstance().initialize { (UserState, error) in
+            if let userState = UserState {
+                switch (UserState) {
+                case .signedIn:
+                    DispatchQueue.main.async {
+                        print("Logged In")
+                    }
+                case .signedOut:
+                    AWSMobileClient.sharedInstance().showSignIn(navigationController: self.navigationController!, { (UserState, error) in
+                        if (error == nil){ //サインイン成功時
+                            DispatchQueue.main.async {
+                                print("Sign In")
+                            }
+                        }
+
+                    })
+                default:
+                    AWSMobileClient.sharedInstance().signOut()
+                }
+                
+                
+                
+ 
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
     }
 
 
